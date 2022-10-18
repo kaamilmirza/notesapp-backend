0e6110c02b287eefbb33283105a9c136b23eb9e3
Kaamil Mirza <kaamil.mirza.2002@gmail.com>
2022-10-18 21:43:08 +0530

    Cleaning up and getting json files to work

--- constants/googleDriveApi.js
+++ constants/googleDriveApi.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly'];
+
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+const API_Key = process.env.G_API_KEY;
--- services/fileService.js
+++ services/fileService.js
@@ -1,58 +1,21 @@
+const { find } = require('../models/file.model.js');
 const File = require('../models/file.model.js');
 //  searches for the file in the db and creates new if doesnt exist 
 // updates the file if it already exists
 module.exports = class FileService{
-    
-    static async getFileList({ filters = null , page = 0, filesPerPage = 20 } = {}) {
-        let query;
-        if (filters) {
-            if ("name" in filters) {
-                query = { name: { $regex: filters["name"], $options: "i" } }
-            } else if ("year" in filters) {
-                query = { year: filters["year"] }
-            } else if ("branch" in filters) {
-                query = { branch: filters["branch"] }
-            } else if ("course" in filters) {
-                query = { course: filters["course"] }
-            } else if ("semester" in filters) {
-                query = { semester: filters["semester"] }
-            } else if ("version" in filters) {
-                query = { version: filters["version"] }
-            } else if ("unit" in filters) {
-                query = { unit: filters["unit"] }
-            } else if ("wdlink" in filters) {
-                query = { wdlink: filters["wdlink"] }
-            }
+    //to get json of all the data collections
+    static async getFileList(body) {
+        try{
+            const files = await File.find();
+            return files;
         }
-        let cursor;
-        try {
-            cursor = await File.find(query);
-        } catch (e) {
-            console.error(`Unable to issue find command, ${e}`)
-            return { file_list: [], total_num_files: 0 }
+        catch(e){
+            console.log(e);
+            return null;
         }
         
     }
-        if (filters) {
-            if ("name" in filters) {
-                cursor = cursor.sort({ name: 1 })
-            } else if ("year" in filters) {
-                cursor = cursor.sort({ year: 1 })
-            } else if ("branch" in filters) {
-                cursor = cursor.sort({ branch: 1 })
-            } else if ("course" in filters) {
-                cursor = cursor.sort({ course: 1 })
-            } else if ("semester" in filters) {
-                cursor = cursor.sort({ semester: 1 })
-            } else if ("version" in filters) {
-                cursor = cursor.sort({ version: 1 })
-            } else if ("unit" in filters) {
-                cursor = cursor.sort({ unit: 1 })
-            } else if ("wdlink" in filters) {
-                cursor = cursor.sort({ wdlink: 1 })
-            }
-        }
-
+    //to create a file with generated g_id and things given in paramenter
     static async createFile(body) {
         let fileDoc = await File.findOne({
             g_id: body.g_id,
@@ -75,12 +38,14 @@ module.exports = class FileService{
             this.updateFile(body);
         }
     }
+    //to update file if it already exists after finding the file
     static async updateFile(body) {
         // console.log("Reached update file..service")
         // console.log(body);
         const fileDoc = await File.findOne({
             g_id: body.g_id,
             });
+        
         if (fileDoc) {
             // console.log("File found");
             // console.log(fileDoc);
