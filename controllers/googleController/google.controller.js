9a2b8521271e6e83d75a3dd1b3d64c64605e3fb3
Kaamil Mirza <kaamil.mirza.2002@gmail.com>
2022-10-26 23:43:56 +0530

    gservices

--- controllers/googleController/google.controller.js
+++ controllers/googleController/google.controller.js
@@ -2,6 +2,16 @@ const googleService = require('../../services/googleService');
 
 module.exports = class googleController {
     static async apiGdrive(req, res, next){
-            
+        try{
+            const driveService = await googleService.getDriveService();
+            const fileId = await googleService.uploadSingleFile(driveService,"Hallticket.pdf","D:/Programminglearning/notesapp-backend/test/");
+            console.log("FileId");
+            console.log(fileId);
+            res.json(fileId);
+        }   
+        catch(error){
+            console.log(error);
+            res.status(500).json(error);
+        }
     }
 }
--- services/googleService.js
+++ services/googleService.js
@@ -1,4 +1,4 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
 const process = require('process');
 const {authenticate} = require('@google-cloud/local-auth');
@@ -10,8 +10,8 @@ const constants = require('../constants/googleDriveApi');
 // If modifying these scopes, delete token.json.
 module.exports = class GoogleService {
 static async getDriveService() {
-  const KEYFILEPATH = path.join(__dirname, 'service.json');
-  const SCOPES = ['https://www.googleapis.com/auth/drive'];
+  const KEYFILEPATH = '../notesapp-364320-a9fd54b361bf.json';
+  const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
 
   const auth = new google.auth.GoogleAuth({
     keyFile: KEYFILEPATH,
@@ -20,32 +20,39 @@ static async getDriveService() {
   const driveService = google.drive({ version: 'v3', auth });
   return driveService;
 };
-static uploadSingleFile = async (fileName, filePath) => {
-    const drive = this.getDriveService;
-    const folderId = 'DRIVE_FOLDER_ID';
+static uploadSingleFile = async (getDriveService,fileName,filePath) => {
+    const drive = getDriveService;
+    const fName = fileName;
+    const fPath = filePath;
+    console.log(fName,fPath);
+    //folderId is the id of the folder inside of the gdrive
+    const folderId = '1VZ5shZrzacC4eDTo03C6Z1Wb0PLrARSg';
+    const readStream = fs.createReadStream(path.join(fPath,fName));
+
     const { data: { id, name } = {} } = await drive.files.create({
       resource: {
-        name: fileName,
+        name: fName,
         parents: [folderId],
       },
       media: {
         mimeType: 'application/pdf',
-        body: fs.createReadStream(filePath),
+        body: readStream,
       },
       fields: 'id,name',
     });
     console.log('File Uploaded', name, id);
+    return id;
   };
   
-  static scanFolderForFiles = async (folderPath) => {
-    const folder = await fs.promises.opendir(folderPath);
-    for await (const dirent of folder) {
-      if (dirent.isFile() && dirent.name.endsWith('.pdf')) {
-        await uploadSingleFile(dirent.name, path.join(folderPath, dirent.name));
-        await fs.promises.rm(filePath);
-      }
-    }
-  };
+  // static scanFolderForFiles = async (folderPath) => {
+  //   const folder = await fs.promises.opendir(folderPath);
+  //   for await (const dirent of folder) {
+  //     if (dirent.isFile() && dirent.name.endsWith('.pdf')) {
+  //       await uploadSingleFile(dirent.name, path.join(folderPath, dirent.name));
+  //       await fs.promises.rm(filePath);
+  //     }
+  //   }
+  // };
 
 }
 
