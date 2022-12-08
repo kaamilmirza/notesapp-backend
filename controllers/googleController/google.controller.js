c6b215d1c9eb916db1e3828ef1986b801cf3df18
Kaamil Mirza <kaamil.mirza.2002@gmail.com>
2022-12-08 23:14:07 +0530

    comments

--- controllers/googleController/google.controller.js
+++ controllers/googleController/google.controller.js
@@ -2,7 +2,9 @@ const googleService = require('../../services/google.service');
 
 module.exports = class googleController {
     static async apiGdrive(req, res, next){
+
         try{
+            //Controller to run google services present under /services/google.service.js
             const driveService = await googleService.getDriveService();
             const fileId = await googleService.scanFolderForFiles(driveService,req.body.filename,req.body.filepath);
             res.json(fileId);
--- controllers/userController/user.controller.js
+++ controllers/userController/user.controller.js
@@ -1,5 +1,6 @@
 
 module.exports = class userController {
+    //controller to find user and create if not present
     static async getUserApi(req,res,next){
         try{
             
