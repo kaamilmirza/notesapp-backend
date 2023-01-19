7e346b2bb27fdaf84d1ee1cbfb75c8c343a1497c
Kaamil Mirza <kaamil.mirza.2002@gmail.com>
2023-01-19 23:13:38 +0530

    Multer fixing

--- middleware/localUpload.js
+++ middleware/localUpload.js
@@ -1,27 +1,23 @@
 const express = require("express")
 const path = require("path")
 const multer = require("multer")
-
-
+const fs = require('fs');
 // var upload = multer({ dest: "Upload_folder_name" })
 // If you do not want to use diskStorage then uncomment it
 module.exports = class localUpload {
     static async uploadAPI(req,res){
         const storage = multer.diskStorage({
-            destination: function(req, file, cb) {
-              cb(null, 'uploads/');
-            },
-            filename: function(req, file, cb) {
-              cb(null, Date.now() + '-' + file.originalname);
-            }
-          });
+          destination : (req,file,cb) => {
+              cb(null,'../pdfStorage/')
+          },
+          filename : (req,file,cb) => {
+            console.log(file);
+            cb(null, Date.now() + path.extname(file.originalname) )
+          },
+        })
 
-        const upload = multer({storage : storage}).single('file');
-        if(!upload){
-            return;
-            res.status(400).send('No file uploaded');
-        }
-        res.send("Uploaded")
+        const upload = multer({storage : storage});
+        upload.single('pdf');
     }
 
   
