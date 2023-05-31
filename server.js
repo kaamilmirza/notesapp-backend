680ebf248640dc79e98ae128f69fb5a19ca2491d
Kaamil Mirza <64482251+kaamilmirza@users.noreply.github.com>
2023-05-31 00:12:46 -0700

    Update server.js : cors

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const cors = require('cors');
 
+app.use(cors()); // Enable CORS for all routes
 app.use(bodyParser.json());
-
 app.use(routes);
 
-
 server.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-  });
+  console.log(`Server running on port ${process.env.PORT}`);
+});
